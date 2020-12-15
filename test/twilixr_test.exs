e0220978e64afec5cf1360fab059bb78644e888b
Kenneth Hunter <kennethdhunter@gmail.com>
2020-12-14 23:11:16 -0700

    Updated test to rely on reading env value instead of hard coded string.

--- test/twilixr_test.exs
+++ test/twilixr_test.exs
@@ -3,7 +3,7 @@ defmodule TwilixrTest do
   doctest Twilixr
 
   test "Client Token retrieved from Config/Config.exs" do
-    assert Twilixr.bot_token() == "2bfvp40ycc0oebzcjp5dhvj7lqovtw"
+    assert Twilixr.bot_token() == Application.get_env(:twilixr, :client_token)
   end
   
 end
