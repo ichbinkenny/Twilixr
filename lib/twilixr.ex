55424b1be573009f57a03956e904dcba5c2b5c1d
Kenneth Hunter <kennethdhunter@gmail.com>
2020-12-15 00:27:02 -0700

    App access tokens can now be requested and revoked.

--- lib/twilixr.ex
+++ lib/twilixr.ex
@@ -3,19 +3,48 @@ defmodule Twilixr do
   Documentation for `Twilixr`.
   """
 
-  @doc """
-  Hello world.
+  def bot_id(), do: Application.get_env(:twilixr, :client_id)
+  def bot_secret(), do: Application.get_env(:twilixr, :client_secret)
+  def bot_scopes(), do: Application.get_env(:twilixr, :needed_scopes)
+  def bot_token(), do: Application.get_env(:twilixr, :access_token)
 
-  ## Examples
+  def base_url(), do: "https://api.twitch.tv/helix/search/"
+  def app_auth_url(), do: "https://id.twitch.tv/oauth2/token?client_id=#{bot_id()}&client_secret=#{bot_secret()}&grant_type=client_credentials"
+  def token_revoke_url(), do: "https://id.twitch.tv/oauth2/revoke?client_id=#{bot_id}&token="
+  def channels_tag(), do: "channels"
+  def query_tag(), do: "?query="
+  def default_headers(), do: ["client-id": bot_id(), "Authorization": "Bearer #{bot_token()}"]
 
-      iex> Twilixr.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def get_app_access_token() do
+    if bot_token() != nil do
+      bot_token()
+    else
+      case HTTPoison.post(app_auth_url(), "", default_headers()) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} -> { 
+          case Poison.decode(~s(#{body})) do
+            {:ok, data} -> IO.puts("Please add the token: #{data["access_token"]} to your config file as access_token: \"TOKEN HERE\",")
+            _ -> IO.puts("I was unable to parse the response received from Twitch.TV. Please try again!")
+          end
+        }
+        _ -> IO.puts("I was unable to get an access code. Please check your provided id and secret in the config file.")
+      end
+    end
   end
 
-  def bot_token(), do: Application.get_env(:twilixr, :client_token)
+  def revoke_app_access_token(token) do
+    case HTTPoison.post(token_revoke_url <> token, "", default_headers) do
+    {:ok, _} -> :ok
+    _ -> :error
+    end
+  end
+
+  def is_streamer_live(_username) do
+
+  end
+
+  def concat_scopes() do
+    {_, scope_str} = Enum.map_reduce(bot_scopes(), "", fn scope, acc -> acc <> "%20" <> scope end)
+    scope_str
+  end
 
 end
--- test/twilixr_test.exs
+++ test/twilixr_test.exs
@@ -3,7 +3,11 @@ defmodule TwilixrTest do
   doctest Twilixr
 
   test "Client Token retrieved from Config/Config.exs" do
-    assert Twilixr.bot_token() == Application.get_env(:twilixr, :client_token)
+    assert Twilixr.bot_secret() == Application.get_env(:twilixr, :client_secret)
+  end
+
+  test "Empty scope concats empty string" do
+    assert Twilixr.concatScopes() == ""
   end
   
 end
